ec84d19f1ebe311b734de5b7bd56f0b8a0584d6d
Brylie Christopher Oxley <brylie@protonmail.com>
2025-03-07 00:44:56 +0200

    update vite.config.ts to refine test file inclusion and exclusion patterns

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
 					name: 'client',
 					environment: 'jsdom',
 					clearMocks: true,
-					include: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+					include: ['src/**/*.{test,spec}.{js,ts}', 'src/**/*.svelte.{test,spec}.{js,ts}'],
 					exclude: ['src/lib/server/**'],
 					setupFiles: ['./vitest-setup-client.ts']
 				}
@@ -28,7 +28,7 @@ export default defineConfig({
 					name: 'server',
 					environment: 'node',
 					include: ['src/**/*.{test,spec}.{js,ts}'],
-					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}']
+					exclude: ['src/lib/server/**']
 				}
 			}
 		]
