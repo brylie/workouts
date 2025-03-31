2b5a00501bfa0bc99b062a392f1d64faf9708c46
Brylie Christopher Oxley <brylie@protonmail.com>
2025-03-31 16:13:42 +0300

    fix: remove prerender export from layout component to streamline rendering process

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -2,7 +2,6 @@
 	import '../app.css';
 	import NavigationMenu from '$lib/components/NavigationMenu.svelte';
 	let { children } = $props();
-	export const prerender = true;
 </script>
 
 <NavigationMenu />
